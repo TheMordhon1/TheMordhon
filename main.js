0c014454cea737b428b6720092c4e790431d9495
themordhon <romdhonm12@gmail.com>
2021-11-18 19:52:54 +0700

    MyWork Updated

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+
+/* Custom JS */
+const mobileNav = document.querySelector('.nav-mobile');
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const toggleOpen = document.querySelector(".toggle-icon-open");
+const toggleClose = document.querySelector(".toggle-icon-close");
+
+navToggle.addEventListener('click', () => {
+	const visibility = mobileNav.getAttribute('data-visible')
+
+	if(visibility === "false") {
+		mobileNav.setAttribute('data-visible', true)
+		navToggle.setAttribute('aria-expanded', true)
+		toggleOpen.classList.add('hidden')
+		toggleClose.classList.remove('hidden')
+	} else if (visibility === "true") {
+		mobileNav.setAttribute('data-visible', false)
+		navToggle.setAttribute('aria-expanded', false)
+		toggleOpen.classList.remove('hidden')
+		toggleClose.classList.add('hidden')
+	}
+});
+
 function updateList() {
 	const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
 		return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
@@ -6,9 +29,12 @@ function updateList() {
 	document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
 	
 	document.querySelectorAll(".nav-dot")[[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
+	
 }
 
 updateList();
 window.addEventListener('scroll', () => {
     updateList();
 })
+
+
